26166f6e3bc41694aa0024a4b5b3715d071f2023
wamidulislam <wamidulislam@gmail.com>
2026-03-07 06:03:01 +0600

    responsibe cards for small device

--- home.js
+++ home.js
@@ -1,6 +1,9 @@
+// card counting dynamically
+const card = document.getElementById("cardCount");
+
 
 // get the element
-const cardContainer = document.getElementById("card")
+const cardContainer = document.getElementById("cards")
 
 async function loadCards() {
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues ")
@@ -9,25 +12,10 @@ async function loadCards() {
     
 }
 
-
-// *{
-// "id": 1,
-// "title": "Fix navigation menu on mobile devices",
-// "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
-// "status": "open",
-// "labels": [
-// "bug",
-// "help wanted"
-// ],
-// "priority": "high",
-// "author": "john_doe",
-// "assignee": "jane_smith",
-// "createdAt": "2024-01-15T10:30:00Z",
-// "updatedAt": "2024-01-15T10:30:00Z"
-// },
-// *
-
 function displayCards(cards){
+    // card count
+    card.innerText = cards.length
+    // loop
     cards.forEach((card)=>{
      const createCard = document.createElement("div")
      createCard.innerHTML = `
